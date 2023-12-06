5499a8a1de7f07cf0bacd151edd79a1a18b098f2
ksenofonus <danilova.ka88@yandex.ru>
2023-12-06 23:23:11 +0400

    feat: add burger menu in the menu page and some improvment

---
+++ coffee-house/assets/js/menu.js
@@ -0,0 +1,25 @@
+'use strict';
+// burger start
+const burgerBtn = document.querySelector('.header-burger');
+const headerMenu = document.querySelector('.menu-wrapper');
+const header = document.querySelector('.header');
+const navItem = document.querySelectorAll('.header_menu-item');
+const inactiveLink = document.querySelector('.link-container');
+
+const toggleBurger = () => {
+  headerMenu.classList.toggle('header_menu__active');
+  burgerBtn.classList.toggle('burger-active');
+}
+burgerBtn.addEventListener('click', () => toggleBurger());
+
+navItem.forEach((item) => {
+  item.addEventListener('click', () => {
+    toggleBurger();
+  })
+})
+
+inactiveLink.addEventListener('click', () => {
+  toggleBurger();
+})
+
+// burger end
