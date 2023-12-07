c10007cc4c852f670422548070ee15b9600fafd8
ksenofonus <danilova.ka88@yandex.ru>
2023-12-07 22:20:00 +0400

    feat: add auto slider

--- coffee-house/assets/js/index.js
+++ coffee-house/assets/js/index.js
@@ -23,6 +23,7 @@ navItem.forEach((item) => {
 //slider start
 const prevBtn = document.querySelector('.left-arrow');
 const nextBtn = document.querySelector('.right-arrow');
+const progress = document.querySelectorAll('.progressbar');
 const control = document.querySelectorAll('.control_item');
 const slider = document.querySelector('.item-wrapper');
 const sliderContainer = document.querySelector('.slider-container');
@@ -31,23 +32,59 @@ let sliderPosition = 0;
 let controlIndex = 0;
 
 const moveToRight = () => {
+  control.forEach((item) => {
+    item.classList.remove('control_item-active');
+  })
   sliderPosition += sliderContainer.offsetWidth;
   if (sliderPosition >= slider.offsetWidth) {
     sliderPosition = 0;
     controlIndex = 0;
   }
-  slider.style.left = -sliderPosition + 'px';
+    slider.style.left = -sliderPosition + 'px';
+    controlIndex += 1;
+  currentControl(controlIndex);
 }
 const moveToLeft = () => {
+  control.forEach((item) => {
+    item.classList.remove('control_item-active');
+  })
   sliderPosition -= sliderContainer.offsetWidth;
   if (sliderPosition < 0) {
     sliderPosition = slider.offsetWidth - sliderContainer.offsetWidth;
     controlIndex = control.length - 1;
   }
-  slider.style.left = -sliderPosition + 'px';
+    slider.style.left = -sliderPosition + 'px';
+  controlIndex -= 1;
+  currentControl(controlIndex);
 }
+
+// const progressMove = () => {
+//   let progessWidth = 1;
+//   let move = setInterval((progress), 6000);
+// }
+
+// const progress = () => {
+//   if (progressWidth >= 100) {
+//     clearInterval(move)
+//   } else {
+//     progessWidth += 1;
+
+//   }
+// }
+
+
+const currentControl = (index) => {
+  control[index].classList.add('control_item-active');
+}
+
+// control.forEach((item, index) => {
+//   item.addEventListener()
+// })
 
 nextBtn.addEventListener('click', () => moveToRight());
 prevBtn.addEventListener('click', () => moveToLeft());
 
+setInterval (() => {
+  moveToRight();
+}, 7000);
 //slider end
