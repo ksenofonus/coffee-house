0daf6f707d47576b4ee13d4343dff7eeecd7216d
ksenofonus <danilova.ka88@yandex.ru>
2024-02-28 13:29:37 +0400

    refactor: refactor menu code

--- coffee-house/assets/js/menu.js
+++ coffee-house/assets/js/menu.js
@@ -38,30 +38,68 @@ const teaBtn = tabs[1];
 const dessertBtn = tabs[2];
 const menuBody = document.querySelector('.menu-container');
 const refreshBtn = document.querySelector('.refresh');
+let productsType = coffee;
+let id = 0;
+let length = 4;
+let wmatch = window.matchMedia("(max-width: 768px)");
 
-// window.addEventListener('load', () => {
-//   getDataProducts();
-//   createCards(coffee);
-//   console.log('load');
-//   if (document.body.clientWidth <= 768) {
-//     console.log('less 768px');
-//     refreshBtn.classList.add('refresh-visible');
-//   }
-// })
+const checkBtn = () => {
+  if (document.body.clientWidth <= 768) {
+    if (productsType.length <= 4) {
+      refreshBtn.classList.remove('refresh-visible');
+    }
+    if (productsType.length <= 4) {
+      refreshBtn.classList.add('refresh-visible');
+    }
+  }
+  
+  if (document.body.clientWidth > 768) {
+    refreshBtn.classList.remove('refresh-visible');
+  }
+}
+checkBtn();
 
-const createCards = (data, length) => {
-  const menuItem = document.querySelectorAll('.menu-item');
-  length = data.length;
-  for (let i = 0; i < length; i++) {
-    menuBody.insertAdjacentHTML('beforeend', `<div class="menu-item opacity"><div class="item-photo"><img src="/assets/images/menu/${data[i].name}.jpg" alt="${data[i].name}"></div><div class="item-description"><div><h3 class="item-title">${data[i].name}</h3><p class="item-content">${data[i].description}</p></div><span class="item-price">${data[i].price}</span></div></div>`);
+const checkProduct = () => {
+  if (id === 0) {
+    productsType = coffee;
+  }
+  if (id === 1) {
+    productsType = tea;
+  }
+  if (id === 2) {
+    productsType = dessert;
   }
 }
 
-// const showCards = (data) => {
-//   createCards(data);
-// }
-if (document.body.clientWidth === 768) {
-  refreshBtn.classList.add('refresh-visible');
+const wchange = () => {
+  wmatch.addEventListener('change', (event) => {
+    checkProduct();
+  if (event.matches) {
+    checkBtn();
+    // refreshBtn.classList.add('refresh-visible');
+    menuBody.replaceChildren('');
+    createCards(productsType);
+  } else {
+    refreshBtn.classList.remove('refresh-visible');
+    menuBody.replaceChildren('');
+    createCards(productsType);
+  }})
+}
+
+
+
+wchange();
+
+const createCards = (products) => {
+  if (document.body.clientWidth <= 768) {
+    length = 4;
+  }
+  if (document.body.clientWidth > 768) {
+    length = productsType.length;
+  }
+  for (let i = 0; i < length; i++) {
+    menuBody.insertAdjacentHTML('beforeend', `<div class="menu-item opacity"><div class="item-photo"><img src="/assets/images/menu/${products[i].name}.jpg" alt="${products[i].name}"></div><div class="item-description"><div><h3 class="item-title">${products[i].name}</h3><p class="item-content">${products[i].description}</p></div><span class="item-price">${products[i].price}</span></div></div>`);
+  }
 }
 
 async function getDataProducts() {
@@ -70,40 +108,22 @@ async function getDataProducts() {
     for (let i = 0; i < data.length; i += 1) {
       if (data[i].category === 'coffee') {
         coffee.push(data[i]);
+        length = coffee.length;
       } else if (data[i].category === 'tea'){
         tea.push(data[i]);
+        length = tea.length;
       } else if (data[i].category === 'dessert'){
         dessert.push(data[i]);
+        length = dessert.length;
       }
     }
-    createCards(coffee, length)
-    switchTabs();
-    window.addEventListener('resize', () => {
-      if (document.body.clientWidth === 768) {
-        console.log('768');
-        refreshBtn.classList.add('refresh-visible');
-        if (productsType.length <= 4) {
-          console.log(productsType);
-          refreshBtn.classList.remove('refresh-visible');
-        }
-        if (productsType.length > 4) {
-          refreshBtn.classList.add('refresh-visible');
-          length = 4;
-          menuBody.replaceChildren(``);
-          createCards(productsType, length);
-        }
-      }
-      if (document.body.clientWidth === 769) {
-        refreshBtn.classList.remove('refresh-visible');
-        length = productsType.length;
-        menuBody.replaceChildren(``);
-        createCards(productsType, length);
-      }
-    })
+    checkProduct();
+    checkBtn();
+    createCards(productsType);
 }
 
 
-const switchTabs = (data) => {
+const switchTabs = (productsType) => {
   tabs.forEach((item) => {
     item.addEventListener('click', () => {
       tabs.forEach((item) => {
@@ -111,30 +131,45 @@ const switchTabs = (data) => {
     });
       item.classList.add('tab-active');
       if (item.id === 'coffee') {
-        data = coffee;
+        productsType = coffee;
+        id = 0;
       }
       if (item.id === 'tea') {
-        data = tea;
+        productsType = tea;
+        id = 1;
       }
       if (item.id === 'dessert') {
-        data = dessert;
+        productsType = dessert;
+        id = 2;
       }
       menuBody.replaceChildren(``);
-      createCards(data, length);
+      createCards(productsType);
+      checkBtn();
   })
 })
 
 }
+switchTabs(productsType);
 
 //switch menu list end
+
+
 getDataProducts();
 
+// const resize = () => {
+//   window.addEventListener('resize', () => {
+//   checkSize();
+//   })
+// }
 
 
 
 
 
 
+
+
+// resize();
 
 
 //products end
