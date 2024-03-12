c2d90a9f6a203404e1f2dfe92910f822378c14ff
ksenofonus <danilova.ka88@yandex.ru>
2024-03-12 12:49:53 +0400

    refactor: add prettier

--- coffee-house/assets/js/menu.js
+++ coffee-house/assets/js/menu.js
@@ -1,55 +1,58 @@
-'use strict';
+"use strict";
 // burger start
-const burgerBtn = document.querySelector('.header-burger');
-const headerMenu = document.querySelector('.menu-wrapper');
-const header = document.querySelector('.header');
-const navItem = document.querySelectorAll('.header_menu-item');
-const inactiveLink = document.querySelector('.link-container');
+const burgerBtn = document.querySelector(".header-burger");
+const headerMenu = document.querySelector(".menu-wrapper");
+const header = document.querySelector(".header");
+const navItem = document.querySelectorAll(".header_menu-item");
+const inactiveLink = document.querySelector(".link-container");
 
 const toggleBurger = () => {
-  headerMenu.classList.toggle('header_menu__active');
-  burgerBtn.classList.toggle('burger-active');
-  document.querySelector('body').classList.toggle('no-scroll');
-}
-burgerBtn.addEventListener('click', () => toggleBurger());
+  headerMenu.classList.toggle("header_menu__active");
+  burgerBtn.classList.toggle("burger-active");
+  document.querySelector("body").classList.toggle("no-scroll");
+};
+burgerBtn.addEventListener("click", () => toggleBurger());
 
 navItem.forEach((item) => {
-  item.addEventListener('click', () => {
+  item.addEventListener("click", () => {
     toggleBurger();
-  })
-})
+  });
+});
 
-inactiveLink.addEventListener('click', () => {
+inactiveLink.addEventListener("click", () => {
   toggleBurger();
-})
+});
 // burger end
 
-
 //products start
-const tabs = document.querySelectorAll('.tabs-item');
-const productsUrl = '../assets/js/products.json';
+const tabs = document.querySelectorAll(".tabs-item");
+const productsUrl = "../assets/js/products.json";
 let coffee = [];
 let tea = [];
 let dessert = [];
 const coffeeBtn = tabs[0];
 const teaBtn = tabs[1];
 const dessertBtn = tabs[2];
-const menuBody = document.querySelector('.menu-container');
-const refreshBtn = document.querySelector('.refresh');
+const menuBody = document.querySelector(".menu-container");
+const refreshBtn = document.querySelector(".refresh");
 let productsType = coffee;
 let id = 0;
 let length = productsType.length;
 const wmatch = window.matchMedia("(max-width: 768px)");
 let i = 0;
-const menuItem = document.querySelectorAll('.menu-item');
+const menuItem = document.querySelectorAll(".menu-item");
 
 //modal start
 const createModal = (products, i) => {
-  document.body.classList.add('no-scroll');
-  document.body.insertAdjacentHTML('afterbegin', `<div class="back"></div>
+  document.body.classList.add("no-scroll");
+  document.body.insertAdjacentHTML(
+    "afterbegin",
+    `<div class="back"></div>
     <div class="modal_wrapper">
     <div class="modal">
-        <div class="item-photo"><img src="../assets/images/menu/${products[i].name.replace(/\s+/g, '')}.jpg" alt="${products[i].name}"></div>
+        <div class="item-photo"><img src="../assets/images/menu/${products[
+          i
+        ].name.replace(/\s+/g, "")}.jpg" alt="${products[i].name}"></div>
         <div class="item-description">
           <div class="item_name">
             <h3 class="item-title">${products[i].name}</h3>
@@ -74,82 +77,91 @@ const createModal = (products, i) => {
             <p class="alert_text">The cost is not final. Download our mobile app to see the final price and place your order. Earn loyalty points and enjoy your favorite coffee with up to 20% discount.</p>
           </div>
           <button class="close_btn">Close</button>
-        </div></div></div>`);
-  const sizeBtnWrap = document.querySelector('.size .prop_tabs');
-  for (let m=0; m < Object.keys(products[i].sizes).length; m++) {
-    sizeBtnWrap.insertAdjacentHTML('beforeend', `<button class="prop_btn" id="${products[i].sizes[Object.keys(products[i].sizes)[m]]['add-price']}">
-    <div class="prop_icon">${Object.keys(productsType[i].sizes)[m].toUpperCase()}</div>
-    <div class="prop_value">${products[i].sizes[Object.keys(products[i].sizes)[m]].size}</div>
-  </button>`)
+        </div></div></div>`
+  );
+  const sizeBtnWrap = document.querySelector(".size .prop_tabs");
+  for (let m = 0; m < Object.keys(products[i].sizes).length; m++) {
+    sizeBtnWrap.insertAdjacentHTML(
+      "beforeend",
+      `<button class="prop_btn" id="${
+        products[i].sizes[Object.keys(products[i].sizes)[m]]["add-price"]
+      }">
+    <div class="prop_icon">${Object.keys(productsType[i].sizes)[
+      m
+    ].toUpperCase()}</div>
+    <div class="prop_value">${
+      products[i].sizes[Object.keys(products[i].sizes)[m]].size
+    }</div>
+  </button>`
+    );
   }
-  const addBtnWrap = document.querySelector('.additives .prop_tabs');
-  for (let m=0; m < products[i].additives.length; m++) {
-    addBtnWrap.insertAdjacentHTML('beforeend', `<button class="prop_btn" id="${products[i].additives[m]['add-price']}">
+  const addBtnWrap = document.querySelector(".additives .prop_tabs");
+  for (let m = 0; m < products[i].additives.length; m++) {
+    addBtnWrap.insertAdjacentHTML(
+      "beforeend",
+      `<button class="prop_btn" id="${products[i].additives[m]["add-price"]}">
     <div class="prop_icon">${m + 1}</div>
     <div class="prop_value">${products[i].additives[m].name}</div>
-  </button>`)
+  </button>`
+    );
   }
   closeModal();
   chooseSize(products, i);
   chooseAdd(products, i);
-  
-}
+};
 const closeModal = () => {
-  const back = document.querySelector('.back');
-  const modalWrapper = document.querySelector('.modal_wrapper')
-  const closeModal = document.querySelector('.close_btn');
+  const back = document.querySelector(".back");
+  const modalWrapper = document.querySelector(".modal_wrapper");
+  const closeModal = document.querySelector(".close_btn");
   const close = () => {
     back.remove();
     modalWrapper.remove();
-    document.body.classList.remove('no-scroll');
-  }
-  back.addEventListener('click', () => close())
-  closeModal.addEventListener('click', () => close())
-  modalWrapper.addEventListener('click', () => close())
-}
+    document.body.classList.remove("no-scroll");
+  };
+  back.addEventListener("click", () => close());
+  closeModal.addEventListener("click", () => close());
+  modalWrapper.addEventListener("click", () => close());
+};
 
 const chooseSize = (products, i) => {
-  const sizeBtns = document.querySelectorAll('.size .prop_btn');
-  sizeBtns[0].classList.add('prop_btn__active');
+  const sizeBtns = document.querySelectorAll(".size .prop_btn");
+  sizeBtns[0].classList.add("prop_btn__active");
   for (let x = 0; x < 3; x++) {
-      sizeBtns[x].addEventListener('click', (event) => {
-        sizeBtns.forEach((size) => {
-          size.classList.remove('prop_btn__active');
-        })
-      sizeBtns[x].classList.add('prop_btn__active');
+    sizeBtns[x].addEventListener("click", (event) => {
+      sizeBtns.forEach((size) => {
+        size.classList.remove("prop_btn__active");
+      });
+      sizeBtns[x].classList.add("prop_btn__active");
       count(products, i, event);
-      }
-      )
+    });
   }
-}
+};
 
 const chooseAdd = (products, i) => {
-  const addBtns = document.querySelectorAll('.additives .prop_btn');
+  const addBtns = document.querySelectorAll(".additives .prop_btn");
   addBtns.forEach((add) => {
-    add.addEventListener('click', (event) => {
-      add.classList.toggle('prop_btn__active');
+    add.addEventListener("click", (event) => {
+      add.classList.toggle("prop_btn__active");
       count(products, i, event);
-      }
-    )
-})
-}
+    });
+  });
+};
 const count = (products, i, event) => {
-  const total = document.querySelector('.total_sum');
-  const btns = document.querySelectorAll('.prop_btn');
+  const total = document.querySelector(".total_sum");
+  const btns = document.querySelectorAll(".prop_btn");
   let startPrice = Number(products[i].price);
   let count = 0;
   let sum = 0;
-  if(event.currentTarget) {
-    for (let i=0; i < btns.length; i++) {
-      if (btns[i].classList.contains('prop_btn__active')) {
+  if (event.currentTarget) {
+    for (let i = 0; i < btns.length; i++) {
+      if (btns[i].classList.contains("prop_btn__active")) {
         count = count + Number(btns[i].id);
       }
     }
     sum = startPrice + count;
-    total.textContent =`$${sum.toFixed(2)}`;
+    total.textContent = `$${sum.toFixed(2)}`;
   }
-}
-
+};
 
 //modal end
 
@@ -163,9 +175,8 @@ const checkProduct = () => {
   if (id === 2) {
     productsType = dessert;
   }
-  return (productsType);
-}
-
+  return productsType;
+};
 
 const checkWidth = () => {
   if (document.body.clientWidth <= 768) {
@@ -175,95 +186,109 @@ const checkWidth = () => {
     length = productsType.length;
   }
   return length;
-}
+};
 
 const checkBtn = () => {
   checkProduct();
   if (document.body.clientWidth <= 768) {
     if (productsType.length <= 4) {
-      refreshBtn.classList.remove('refresh-visible');
+      refreshBtn.classList.remove("refresh-visible");
     }
     if (productsType.length > 4) {
-      refreshBtn.classList.add('refresh-visible');
+      refreshBtn.classList.add("refresh-visible");
     }
   }
   if (document.body.clientWidth > 768) {
-    refreshBtn.classList.remove('refresh-visible');
+    refreshBtn.classList.remove("refresh-visible");
   }
-}
+};
 checkBtn();
 
 const wchange = () => {
-  wmatch.addEventListener('change', (event) => {
-  if (event.matches) {
-    checkProduct();
-    checkBtn();
-    checkWidth();
-    refreshBtn.classList.add('refresh-visible');
-    menuBody.replaceChildren('');
-    createCards(productsType, 0, length);
-  } else {
-    checkProduct();
-    console.log(productsType);
-    checkWidth();
-    refreshBtn.classList.remove('refresh-visible');
-    length = productsType.length;
-    createCards(productsType, 4, length);
-  }})
-}
+  wmatch.addEventListener("change", (event) => {
+    if (event.matches) {
+      checkProduct();
+      checkBtn();
+      checkWidth();
+      refreshBtn.classList.add("refresh-visible");
+      menuBody.replaceChildren("");
+      createCards(productsType, 0, length);
+    } else {
+      checkProduct();
+      console.log(productsType);
+      checkWidth();
+      refreshBtn.classList.remove("refresh-visible");
+      length = productsType.length;
+      createCards(productsType, 4, length);
+    }
+  });
+};
 wchange();
 
 const createCards = (products, i, length) => {
   for (i; i < length; i++) {
-    menuBody.insertAdjacentHTML('beforeend', `<div class="menu-item opacity"><div class="item-photo"><img src="../assets/images/menu/${products[i].name.replace(/\s+/g, '')}.jpg" alt="${products[i].name}"></div><div class="item-description"><div><h3 class="item-title">${products[i].name}</h3><p class="item-content">${products[i].description}</p></div><span class="item-price">${products[i].price}</span></div></div>`);
+    menuBody.insertAdjacentHTML(
+      "beforeend",
+      `<div class="menu-item opacity"><div class="item-photo"><img src="../assets/images/menu/${products[
+        i
+      ].name.replace(/\s+/g, "")}.jpg" alt="${
+        products[i].name
+      }"></div><div class="item-description"><div><h3 class="item-title">${
+        products[i].name
+      }</h3><p class="item-content">${
+        products[i].description
+      }</p></div><span class="item-price">${
+        products[i].price
+      }</span></div></div>`
+    );
   }
-  const menuItem = document.querySelectorAll('.menu-item');
+  const menuItem = document.querySelectorAll(".menu-item");
 
   menuItem.forEach((item) => {
-    item.addEventListener('click', () => {
+    item.addEventListener("click", () => {
       let j = Array.from(menuItem).indexOf(item);
       createModal(products, j);
     });
-    })
-  }
+  });
+};
 
 async function getDataProducts() {
-    const response = await fetch(productsUrl);
-    const data = await response.json();
-    for (let i = 0; i < data.length; i += 1) {
-      if (data[i].category === 'coffee') {
-        coffee.push(data[i]);
-        length = coffee.length;
-      } else if (data[i].category === 'tea'){
-        tea.push(data[i]);
-        length = tea.length;
-      } else if (data[i].category === 'dessert'){
-        dessert.push(data[i]);
-        length = dessert.length;
-      }
+  const response = await fetch(productsUrl);
+  const data = await response.json();
+  for (let i = 0; i < data.length; i += 1) {
+    if (data[i].category === "coffee") {
+      coffee.push(data[i]);
+      length = coffee.length;
+    } else if (data[i].category === "tea") {
+      tea.push(data[i]);
+      length = tea.length;
+    } else if (data[i].category === "dessert") {
+      dessert.push(data[i]);
+      length = dessert.length;
     }
-    checkProduct();
-    checkBtn();
-    checkWidth();
-    createCards(productsType, 0, length);
+  }
+  checkProduct();
+  checkBtn();
+  checkWidth();
+  createCards(productsType, 0, length);
 }
 
 const switchTabs = (productsType) => {
   tabs.forEach((item) => {
-    item.addEventListener('click', () => {
+    item.addEventListener("click", () => {
       tabs.forEach((item) => {
-        item.classList.remove('tab-active');
-    });
-      item.classList.add('tab-active');
-      if (item.id === 'coffee') {
+        item.classList.remove("tab-active");
+      });
+      item.classList.add("tab-active");
+      if (item.id === "coffee") {
         productsType = coffee;
         id = 0;
       }
-      if (item.id === 'tea') {
+      if (item.id === "tea") {
         productsType = tea;
         id = 1;
       }
-      if (item.id === 'dessert') {
+      if (item.id === "dessert") {
         productsType = dessert;
         id = 2;
       }
@@ -272,25 +297,22 @@ const switchTabs = (productsType) => {
       checkWidth();
       createCards(productsType, 0, length);
       checkBtn();
-  })
-})
-
-}
+    });
+  });
+};
 switchTabs(productsType);
 
 //switch menu list end
-
 
 getDataProducts();
 
 //refresh button start
 
 //refresh button end
-refreshBtn.addEventListener('click', () => {
+refreshBtn.addEventListener("click", () => {
   checkProduct();
   length = productsType.length;
   createCards(productsType, 4, length);
-  refreshBtn.classList.remove('refresh-visible');
-})
+  refreshBtn.classList.remove("refresh-visible");
+});
 //products end
-
